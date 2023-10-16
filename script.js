e36b611d8ec5b2878c06ffa8e08ac8f5e5402ecb
Arisha <arishaaa333@gmail.com>
2023-10-16 20:22:08 +0530

    update files

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-const projectCards = document.querySelectorAll('.project-card');
+const button = document.querySelector('#nav-mobile');
+const navLinks = document.querySelector('.links');
 
-let currentIndex = 0;
-
-// show slide
-function showSlide(index){
-    projectCards.forEach((card) => {
-        card.style.display = 'none';
-    });
-    
-    projectCards[index].style.display = 'block';
-}
-
-// next slide
-function nextSlide(){
-    currentIndex = (currentIndex + 1) % projectCards.length;
-    showSlide(currentIndex);
-}
-
-// prev slide 
-function prevSlide(){
-    currentIndex = (currentIndex - 1 + projectCards.length) % projectCards.length;
-    showSlide(currentIndex);
-}
-
-// show the first slide initially
-showSlide(currentIndex);
-
-// set up event listeners on buttons
-document.querySelector('.next').addEventListener('click', nextSlide);
-document.querySelector('.prev').addEventListener('click', prevSlide);
+button.addEventListener('click', function(){
+    navLinks.classList.toggle('active');
+})
