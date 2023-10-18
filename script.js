5c7b80ff7014ceccb5ce97926bd87ef31fab06cd
Arisha <arishaaa333@gmail.com>
2023-10-18 21:46:23 +0530

    add top button

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const button = document.querySelector('#nav-mobile');
-const navLinks = document.querySelector('.links');
+const button = document.querySelector("#nav-mobile");
+const navLinks = document.querySelector(".links");
 
-button.addEventListener('click', function(){
-    navLinks.classList.toggle('active');
+// navbar toggle
+const navBtn = document.querySelector('#nav-mobile');
+navBtn.addEventListener('click', function(){
+    console.log("hey");
 })
+
+// function to show top button
+const topBtn = document.querySelector(".top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// function to move to top
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
